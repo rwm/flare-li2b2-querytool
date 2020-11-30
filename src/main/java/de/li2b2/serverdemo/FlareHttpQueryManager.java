8058436a366f8c6f05df0234f1e2e5482c988a51
R.W. Majeed <rwmajeed@gmail.com>
2020-11-30 11:31:59 +0100

    execute queries with FLARE-executor

--- src/main/java/de/li2b2/serverdemo/FlareHttpQueryManager.java
+++ src/main/java/de/li2b2/serverdemo/FlareHttpQueryManager.java
@@ -2,24 +2,34 @@ package de.li2b2.serverdemo;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.UncheckedIOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.xml.bind.JAXB;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
-import de.sekmi.li2b2.api.crc.QueryResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import de.sekmi.li2b2.api.crc.QueryStatus;
 import de.sekmi.li2b2.services.impl.crc.ExecutionImpl;
 import de.sekmi.li2b2.services.impl.crc.FileBasedQueryManager;
 import de.sekmi.li2b2.services.impl.crc.QueryImpl;
-import de.sekmi.li2b2.services.impl.crc.ResultImpl;
 
 /**
  * Implementation of query manager which forwards
@@ -30,6 +40,7 @@ import de.sekmi.li2b2.services.impl.crc.ResultImpl;
  *
  */
 public class FlareHttpQueryManager extends FileBasedQueryManager{
+	private static final Logger log = Logger.getLogger(FlareHttpQueryManager.class.getName());
 	@XmlElement
 	private URI flareBaseURI;
 
@@ -45,45 +56,71 @@ public class FlareHttpQueryManager extends FileBasedQueryManager{
 	}
 	@Override
 	protected void executeQuery(QueryImpl query){
-		ExecutionImpl e = query.addExecution(QueryStatus.INCOMPLETE);
+		ExecutionImpl exec = query.addExecution(QueryStatus.INCOMPLETE);
 		
 		// print query
 		System.out.println("Query "+query.getId());
 		try {
 			System.out.println(Util.elementToXmlString(query.getDefinition(), true));
 			System.out.println("Wrapped");
-			System.out.println(Util.elementToXmlString(Util.wrapRequestQueryDefinition(query.getDefinition(), query.getRequestTypes()), true));
+			
 		} catch (TransformerException e1) {
 			e1.printStackTrace();
 		}
-
 		
 		// perform execution, customized execution can be done here 
-		e.setStartTimestamp(Instant.now());
+		exec.setStartTimestamp(Instant.now());
 
-		HttpURLConnection connection;
+		HttpURLConnection c;
+		Document response = null;
 		try {
-			connection = (HttpURLConnection)flareBaseURI.toURL().openConnection();
-			connection.disconnect();
-		} catch (MalformedURLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		
-		for( QueryResult result : e.getResults() ) {
-			ResultImpl ri = (ResultImpl)result;
-			switch( result.getResultType() ) {
-			case "PATIENT_COUNT_XML":
-				ri.fillWithPatientCount(2342);
+			String requestBody = Util.elementToXmlString(Util.wrapRequestQueryDefinition(query.getDefinition(), query.getRequestTypes()), true);
+			c = (HttpURLConnection)flareBaseURI.toURL().openConnection();
+			c.setDoOutput(true);
+			c.setDoInput(true);
+			c.setRequestMethod("POST");
+			c.setRequestProperty("Content-type", "application/xml; charset=utf-8");
+			try( OutputStreamWriter w = new OutputStreamWriter(c.getOutputStream(), StandardCharsets.UTF_8) ){
+				w.write(requestBody);
 			}
+			if( c.getResponseCode() == 200 ) {
+				try( InputStream in = c.getInputStream() ){
+					DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+					response = builder.parse(in);
+				}
+			}else {
+				log.warning("Upstream FHIR server returned status code "+c.getResponseCode());
+			}
+			c.disconnect();
+		} catch (MalformedURLException e) {
+			throw new IllegalStateException("Illegal FLARE base URI: "+flareBaseURI.toString());
+		} catch (IOException e) {
+			log.log(Level.WARNING, "IO error during request processing", e);
+			exec.setStatus(QueryStatus.ERROR);
+		} catch (TransformerException | ParserConfigurationException | SAXException e) {
+			log.log(Level.WARNING, "XML error during request or response processing", e);
+			exec.setStatus(QueryStatus.ERROR);
 		}
-
-		e.setEndTimestamp(Instant.now());
-		
-		e.setStatus(QueryStatus.FINISHED);
+		if( response != null ) {
+			String count = getElementValueAttribute(response, "patient_count");
+			double start = Double.parseDouble(getElementValueAttribute(response, "start_time"));
+			double end = Double.parseDouble(getElementValueAttribute(response, "end_time"));
+			
+			exec.setStartTimestamp(Instant.ofEpochMilli((long)(start*1000)));
+			exec.setEndTimestamp(Instant.ofEpochMilli((long)(end*1000)));
+			exec.getResult("PATIENT_COUNT_XML").fillWithPatientCount(Integer.parseInt(count));
+			
+			exec.setStatus(QueryStatus.FINISHED);
+		}else {
+			exec.setStatus(QueryStatus.ERROR);
+		}
+	}
+	private static String getElementValueAttribute(Document dom, String elementName) {
+		NodeList nl = dom.getElementsByTagName(elementName);
+		if( nl.getLength() == 0 ) {
+			return null;
+		}
+		return ((Element)nl.item(0)).getAttribute("value");
 	}
 
 	public static FlareHttpQueryManager initialize(Path previousStateFile, Path queryDir) {
